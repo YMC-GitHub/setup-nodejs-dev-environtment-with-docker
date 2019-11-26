aa51178a849cfac37bca436cf1a39c74139b58e2
yemiancheng <ymc.github@gmail.com>
2019-11-26 08:32:15 +0800

    core(feat):using install deps task

--- task.sh
+++ task.sh
@@ -5,14 +5,20 @@ THIS_FILE_PATH=$(cd `dirname $0`; pwd)
 source ${THIS_FILE_PATH}/conf.sh
 
 # 安装一个类库
-#docker exec -it -w $project_path_in_vm npm install lodash
+docker exec -it -w $project_path_in_vm $codes_container_name npm install lodash
 #docker exec -it -w /project project npm install
 #docker exec -it -w $project_path_in_vm $codes_container_name npm install
 
 # 列出模块文件
 #docker exec -it project ls /project
 #docker exec -it -w /project project ls
+echo "list file in CM ..."
 docker exec -it --workdir $project_path_in_vm $codes_container_name ls
+echo "list file in CM node_modules ..."
+docker exec -it --workdir $project_path_in_vm $codes_container_name ls node_modules
+# 生成部署源码build
+docker exec -it --workdir $project_path_in_vm $codes_container_name npm run build
+echo "list file in PM or CM ..."
 
 # 执行某一命令
 #2 运行开发源码文件
