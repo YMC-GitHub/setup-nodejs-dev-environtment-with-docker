420429d044793b9514cf320056d05f0643931422
yemiancheng <ymc.github@gmail.com>
2019-11-28 13:04:31 +0800

    feat(core): renaming file for pro

--- init/index.sh
+++ init/index.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/sh
 
-THIS_FILE_PATH=$(cd `dirname $0`; pwd)
-source ${THIS_FILE_PATH}/init/build_and_run_deps_image.sh
+THIS_FILE_PATH=$(
+  cd $(dirname $0)
+  pwd
+)
+source ${THIS_FILE_PATH}/init/create_deps_volume.sh
 source ${THIS_FILE_PATH}/init/build_and_run_code_image.sh
 source ${THIS_FILE_PATH}/init/copy_from_host_to_vm.sh
-
